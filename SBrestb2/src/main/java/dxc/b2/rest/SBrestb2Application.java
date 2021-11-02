5f436a284ad0b227e900662b7534609c0d5bef2f
pkumara5 <pkumara5@IN-PG02K5MX>
2021-11-02 02:16:11 -0700

    initial commit - student rest

---
+++ SBrestb2/src/main/java/dxc/b2/rest/SBrestb2Application.java
@@ -0,0 +1,20 @@
+package dxc.b2.rest;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBrestb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBrestb2Application.class, args);
+	}
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
